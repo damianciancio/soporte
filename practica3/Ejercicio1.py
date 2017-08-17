ba53d7f4ace5616465dc19dc7d133b85feb78354
Emiliano <emirod1983@gmail.com>
2017-08-17 01:11:50 -0300

    Comentarios y parches a la practica 3, no pude compilarla

--- practica3/Ejercicio1.py
+++ practica3/Ejercicio1.py
@@ -1,15 +1,7 @@
-# conn = pymysql.connect(host='localhost', port=3306, user='root', password='', db='personas') conecccion laboratorio facu
-
 import pymysql
 import datetime
 
-def get_persona_from_console():
-    nom = str(input("Ingrese un nombre: "))
-    fechaNac = str(input("Ingrese fecha de nacimiento(yyyy-mm-dd): "))
-    dni = input("Ingrese el documento: ")
-    altura = input("Ingrese altura: ")
-    return Persona(nom,fechaNac,dni,altura)
-
+#<editor-fold desc="Clases que representan las entidades de la BDD">
 class Persona():
     nombre = ""
     fechaNac = datetime.date(2000,1,1)
@@ -33,20 +25,43 @@ class Peso():
         self.idPersona = idPersona
         self.peso = peso
         self.fecha = fecha
-
+#</editor-fold>
 
 def conectar():
-   global conn, cur
-   conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='test')
-   cur = conn.cursor()
-   return cur
+    """
+    Conecta a la base de datos especificada en conn.
+    :return: Un cursor apuntando a la base de datos.
+    """
+    global conn, cur
+    conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='test')
+    #[conexion laboratorio facu] conn = pymysql.connect(host='localhost', port=3306, user='root', password='', db='personas')
+    cur = conn.cursor()
+    return cur
 
 def desconectar():
-   conn.commit()
-   cur.close()
-   conn.close()
+    """
+    Cierra la conexion a la base de datos.
+    """
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_persona_from_console():
+    """
+    Crea un objeto Persona a partir de informacion ingresada por el usuario.
+    :return: El objeto Persona creado.
+    """
+    nom = str(input("Ingrese un nombre: "))
+    fechaNac = str(input("Ingrese fecha de nacimiento(yyyy-mm-dd): "))
+    dni = input("Ingrese el documento: ")
+    altura = input("Ingrese altura: ")
+    return Persona(nom,fechaNac,dni,altura)
 
 def select_all():
+    """
+    Busca todos los elementos de la tabla personas, los mapea a objetos y los almacena en un arreglo.
+    :return: El arreglo con todas las personas.
+    """
     query = "select * from persona"
     try:
         cur = conectar()
@@ -61,15 +76,25 @@ def select_all():
     return d
 
 def insert_row(persona):
-   query = "INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES (%s, %s, %s, %s)"
-   try:
+    """
+    Ejercicio 2.
+    Inserta en la base de datos una persona.
+    :param persona: La persona a ingresar a la base de datos.
+    """
+    query = "INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES (%s, %s, %s, %s)"
+    try:
         cur = conectar()
         cur.execute(query, (persona.nombre, persona.fechaNac, persona.dni, persona.altura))
         desconectar()
-   except Exception:
-       pass
+    except Exception:
+        pass
 
 def delete_persona(persona):
+    """
+    Ejercicio 3.
+    Elimina de la base de datos una persona.
+    :param persona: La persona a eliminar de la base de datos.
+    """
     try:
         conectar()
         query = "delete from persona where idpersona = %s"
@@ -79,6 +104,12 @@ def delete_persona(persona):
         pass
 
 def get_persona_byDNI(persona):
+    """
+    Ejercicio 4.
+    Buscar en la base de datos una persona segun su DNI.
+    :param persona: La persona a buscar segun su DNI.
+    :return: La persona encontrada.
+    """
     found = None
     try:
        conectar()
@@ -93,15 +124,27 @@ def get_persona_byDNI(persona):
         pass
 
 def update_persona(persona):
+    """
+    Ejercicio 5.
+    Buscar en la base de datos una persona y actualizar su informacion. Luego imprime esos datos.
+    :param persona: Persona con los datos actualizados, para ingresar a la base de datos.
+    :return: 
+    """
     try:
         conectar()
         query = "update persona set nombre = %s, fechaNacimiento= %s, dni=%s, altura=%s where idpersona = %s"
         cur.execute(query, (persona.nombre , persona.fechaNac, persona.dni, persona.altura, persona.id))
         desconectar()
+        print("Datos de la persona: %s, %s, %s, %s", persona.nombre,persona.fechaNac, persona.dni, persona.altura)
     except Exception:
         pass
 
 def get_all_personas_to_list():
+    """
+    Ejercicio 6.
+    Obtiene todos los registros de la tabla personas y los almacena en una lista.
+    :return: La lista de personas.
+    """
     lista = []
     try:
         conectar()
@@ -114,6 +157,12 @@ def get_all_personas_to_list():
     return lista
 
 def add_peso(persona, peso):
+    """
+    Ejercicio 8.
+    Ingresa un registro en la tabla PersonaPeso, luego de ejecutar validaciones.
+    :param persona: Persona de quien se va a insertar un nuevo registro.
+    :param peso: Nuevo peso a registrar.
+    """
     try:
         persona = get_persona_byDNI(persona)
         date = peso.fecha
@@ -133,8 +182,13 @@ def add_peso(persona, peso):
     except Exception:
         pass
 
-
 def pesos_por_persona(persona):
+    """
+    Ejercicio 10.
+    Muestra el historial de registros de pesos de una persona.
+    :param persona: Persona sobre la cual se realizara la consulta.
+    :return: **********REVISAR*****************
+    """
     try:
         persona = get_persona_byDNI(persona)
         query = "select * from personapeso where idpersona = %s"
@@ -149,6 +203,10 @@ def pesos_por_persona(persona):
     return persona
 
 def list_persona_pesos():
+    """
+    ******************REVISAR****************
+    :return: 
+    """
     personas= []
     try:
         query = "select * from persona left join personapeso on persona.idpersona = personapeso.idpersona"
@@ -169,57 +227,61 @@ def list_persona_pesos():
     return personas
 
 def menu():
-   print("***MENU***")
-   print("1 . Listar personas.")
-   print("2 . Insertar persona.")
-   print("3 . Eliminar persona.")
-   print("4 . Buscar por DNI.")
-   print("5 . Actualizar persona.")
-   print("6 . Devolver en listas.")
-   print("7 . Insertar PersonaPeso y validar.")
-   print("8 . Buscar por DNI y mostrar pesos.")
-   print("9 . Devolver todo de todo.")
-   print()
-   opt = int(input("Ingrese una opcion: "))
+    """
+    Interfaz principal del programa, donde se pueden ejecutar todas las consultas
+    requeridas por la practica.
+    """
+    print("***MENU***")
+    print("1 . Listar personas.")
+    print("2 . Insertar persona.")
+    print("3 . Eliminar persona.")
+    print("4 . Buscar por DNI.")
+    print("5 . Actualizar persona.")
+    print("6 . Devolver en listas.")
+    print("7 . Insertar PersonaPeso y validar.")
+    print("8 . Buscar por DNI y mostrar pesos.")
+    print("9 . Devolver todo de todo.")
+    print()
+    opt = int(input("Ingrese una opcion: "))
 
-   if (opt==1):
+    if (opt==1):
        print(select_all())
-   elif (opt == 2):
-       insert_row(get_persona_from_console())
-   elif (opt == 3):
-       dni = int(input("Ingrese el dni de la persona a eliminar: "))
-       persona = get_persona_byDNI(Persona("","",dni,0))
-       delete_persona(persona)
-   elif (opt == 4):
-       dni = int(input("Ingrese el dni de la persona a mostrar: "))
-       persona = get_persona_byDNI(Persona("","",dni,0))
-       print("ID Nombre  FechaNac      DNI   Altura")
-       print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
-   elif (opt == 5):
-       dni = int(input("Ingrese el dni de la persona a modificar: "))
-       persona_new_data = get_persona_from_console()
-       persona_new_data.id = get_persona_byDNI(Persona("","",dni,0)).id
-       update_persona(persona_new_data)
-   elif (opt == 6):
-       print(get_all_personas_to_list())
-   elif (opt == 7):
-       dni = int(input("Ingrese el numero de documento: "))
-       fecha = datetime.datetime.strptime(str(input("Ingrese la fecha del nuevo peso (yyyy-mm-dd): ")), "%Y-%m-%d")
-       peso = float(input("Ingrese el peso: "))
-       add_peso(Persona("","",dni,0),Peso(0,peso, fecha))
-   elif (opt == 8):
-       dni = int(input("Ingrese el dni de la persona a listar: "))
-       persona = pesos_por_persona(Persona("",None,dni,0))
-       print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
-       for i in persona.pesos:
-           print(i.fecha, i.peso)
-   elif (opt == 9):
-       personas = list_persona_pesos()
-       for persona in personas:
-           print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
-           for peso in persona.pesos:
-               print(peso.fecha, peso.peso)
-   else:
-       menu()
+    elif (opt == 2):
+        insert_row(get_persona_from_console())
+    elif (opt == 3):
+        dni = int(input("Ingrese el dni de la persona a eliminar: "))
+        persona = get_persona_byDNI(Persona("","",dni,0))
+        delete_persona(persona)
+    elif (opt == 4):
+        dni = int(input("Ingrese el dni de la persona a mostrar: "))
+        persona = get_persona_byDNI(Persona("","",dni,0))
+        print("ID Nombre  FechaNac      DNI   Altura")
+        print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
+    elif (opt == 5):
+        dni = int(input("Ingrese el dni de la persona a modificar: "))
+        persona_new_data = get_persona_from_console()
+        persona_new_data.id = get_persona_byDNI(Persona("","",dni,0)).id
+        update_persona(persona_new_data)
+    elif (opt == 6):
+        print(get_all_personas_to_list())
+    elif (opt == 7):
+        dni = int(input("Ingrese el numero de documento: "))
+        fecha = datetime.datetime.strptime(str(input("Ingrese la fecha del nuevo peso (yyyy-mm-dd): ")), "%Y-%m-%d")
+        peso = float(input("Ingrese el peso: "))
+        add_peso(Persona("","",dni,0),Peso(0,peso, fecha))
+    elif (opt == 8):
+        dni = int(input("Ingrese el dni de la persona a listar: "))
+        persona = pesos_por_persona(Persona("",None,dni,0))
+        print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
+        for i in persona.pesos:
+            print(i.fecha, i.peso)
+    elif (opt == 9):
+        personas = list_persona_pesos()
+        for persona in personas:
+            print(persona.id, persona.nombre, persona.fechaNac, persona.dni, persona.altura)
+            for peso in persona.pesos:
+                print(peso.fecha, peso.peso)
+    else:
+        menu()
 
 menu()
