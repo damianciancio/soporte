37b223834ab8a023ee26c7c5521f52a2b1f69372
Emiliano <kid_Arage@msn.com>
2017-08-18 17:31:22 -0300

    Información ficticia para popular las tablas.

---
+++ practica3/MockData.sql
@@ -0,0 +1,20 @@
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Maicol', '2000-12-12', 33333330, 185);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Juan', '1996-12-12', 33333331, 172);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Pepe', '1995-12-12', 33333332, 177);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Aymara', '2001-12-12', 33333333, 166);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Melina', '2002-12-12', 33333334, 188);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Juana', '2003-12-12', 33333335, 181);
+INSERT INTO persona(nombre, fechaNacimiento, dni, altura) VALUES ('Matias', '2004-12-12', 33333336, 182);
+
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (1,'2015-05-15',90);
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (1,'2015-05-17',91);
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (1,'2015-05-19',90);
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (1,'2015-05-24',88);
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (5,'2015-05-15',67);
+INSERT INTO personapeso(idpersona,fecha,peso) VALUES (5,'2015-05-17',63);
+
+select * from persona;
+
+SELECT * FROM personapeso;
+
+select * from persona left join personapeso on persona.idpersona = personapeso.idpersona;
