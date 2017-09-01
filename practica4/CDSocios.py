a714738ad5777ee8fb8d2355907c13ecd61c9905
Emiliano <kid_Arage@msn.com>
2017-09-01 01:45:22 -0300

    Comentarios y algo de refactoreo en la practica 4

--- practica4/CDSocios.py
+++ practica4/CDSocios.py
@@ -4,21 +4,29 @@ from pymysql import *
 
 class CDSocios():
 
-    def alta(self,socio):
-       consulta = "INSERT INTO socios(dni, nombre, apellido) VALUES (%s, %s, %s)"
-       try:
+    def alta_socio(self,socio):
+        """
+        Inserta un nuevo socio en la base de datos.
+        :param socio: Objeto Socio a insertar.
+        :return: 
+        """
+        consulta = "INSERT INTO socios(dni, nombre, apellido) VALUES (%s, %s, %s)"
+        try:
             datos = AccesoDatos()
             cur = datos.conectar()
             cur.execute(consulta, (socio.dni, socio.nombre, socio.apellido))
             datos.desconectar()
-       except OperationalError as e:
+        except OperationalError as e:
             raise Exception("ERROR FATAL")
-       except Exception as a:
+        except Exception as a:
             raise Exception("Error al conectar a la base de datos")
 
-
-
-    def modificacion(self,socio):
+    def modificar_socio(self,socio):
+        """
+        Modifica un socio de la base de datos.
+        :param socio: Objeto Socio con los nuevos valores a guardar.
+        :return: 
+        """
         consulta = "UPDATE socios SET socios.dni = %s, socios.nombre = %s, socios.apellido = %s" \
                    "WHERE socios.id = %s"
         try:
@@ -31,7 +39,12 @@ class CDSocios():
         except Exception as a:
             raise Exception("Error al conectar a la base de datos")
 
-    def baja(self,socio):
+    def baja_socio(self,socio):
+        """
+        Elimina un socio en la base de datos.
+        :param socio: Objeto Socio a eliminar.
+        :return: 
+        """
         consulta = "DELETE FROM socios WHERE idsocio = %s"
         try:
             datos = AccesoDatos()
@@ -43,8 +56,12 @@ class CDSocios():
         except Exception as a:
             raise Exception("Error al conectar a la base de datos")
 
-
     def buscar_por_id(self,socio):
+        """
+        Busca un socio en la base de datos.
+        :param socio: Objeto Socio a buscar.
+        :return: El socio si es encontrado, None si no lo encuentra.
+        """
         consulta = "SELECT * FROM socios WHERE idsocio = %s"
         encontrado = None
         try:
@@ -60,8 +77,11 @@ class CDSocios():
             raise Exception("Error al conectar a la base de datos")
         return encontrado
 
-
     def buscar_todos(self):
+        """
+        Busca todos los socios y su informacion de la base de datos.
+        :return: Una lista con todos los objetos Socio obtenidos.
+        """
         consulta = "SELECT * FROM socios"
         encontrados = []
         try:
@@ -79,6 +99,11 @@ class CDSocios():
         return encontrados
 
     def siguiente(self,id):
+        """
+        Dada una id de socio, obtiene el socio con la siguiente id.
+        :param id: La id a buscar.
+        :return: El siguiente socio segun su id.
+        """
         consulta = "select * from socios m " \
                    "where m.idsocio = (select min(idsocio) from socios s " \
                    "where s.idsocio > %s);"
@@ -97,6 +122,11 @@ class CDSocios():
         return encontrado
 
     def anterior(self,id):
+        """
+        Dada una id de socio, obtiene el socio con la id anterior.
+        :param id: La id a buscar.
+        :return: El socio anterior segun su id.
+        """
         consulta = "select * from socios m " \
                    "where m.idsocio = (select min(idsocio) from socios s " \
                    "where s.idsocio < %s);"
@@ -114,13 +144,16 @@ class CDSocios():
             raise Exception("Error al conectar a la base de datos")
         return encontrado
 
-    def primero(self,id):
+    def primero(self):
+        """
+        Encuentra el socio con la menor id.
+        :return: El socio encontrado.
+        """
         consulta = "select min(idsocio) from socios"
-        encontrado = None
         try:
             datos = AccesoDatos()
             cur = datos.conectar()
-            cur.execute(consulta,(id))
+            cur.execute(consulta)
             d = cur.fetchone()
             encontrado = Socio(d[1],d[2],d[3],d[0])
             datos.desconectar()
@@ -130,13 +163,16 @@ class CDSocios():
             raise Exception("Error al conectar a la base de datos")
         return encontrado
 
-    def ultimo(self,id):
+    def ultimo(self):
+        """
+        Encuentra el socio con la mayor id.
+        :return: El socio encontrado.
+        """
         consulta = "select max(idsocio) from socios"
-        encontrado = None
         try:
             datos = AccesoDatos()
             cur = datos.conectar()
-            cur.execute(consulta,(id))
+            cur.execute(consulta)
             d = cur.fetchone()
             encontrado = Socio(d[1],d[2],d[3],d[0])
             datos.desconectar()
@@ -145,9 +181,13 @@ class CDSocios():
         except Exception as a:
             raise Exception("Error al conectar a la base de datos")
         return encontrado
-
 
     def contarDNI(self, dniSocio):
+        """
+        Busca la cantidad de socios con el numero de DNI dado.
+        :param dniSocio: Numero de DNI a usar como parametro de busqueda.
+        :return: El dni encontrado y la cantidad de repeticiones de su valor.
+        """
         consulta = "select count(*)from socios where dni=%s"
         encontrado = None
         try:
