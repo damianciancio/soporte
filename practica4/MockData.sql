0e19469505c8438eeb36c9f57d6ed48249e23f7b
Emiliano <kid_Arage@msn.com>
2017-09-01 15:11:23 -0300

    Agregado un menu a la practica 4 y otras correcciones de codigo. Esto ya esta para entregar.

---
+++ practica4/MockData.sql
@@ -0,0 +1,5 @@
+INSERT INTO socios(dni, nombre, apellido) VALUES (40000000, 'Angel', 'Di Maria');
+INSERT INTO socios(dni, nombre, apellido) VALUES (40000001, 'Luciana', 'Aymar');
+INSERT INTO socios(dni, nombre, apellido) VALUES (40000002, 'Belen', 'Casetta');
+INSERT INTO socios(dni, nombre, apellido) VALUES (40000003, 'Luis', 'Scola');
+INSERT INTO socios(dni, nombre, apellido) VALUES (40000004, 'Peque', 'Pareto');
